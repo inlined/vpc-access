8683e49eb4f353f952833787fa5109e5b441d638
Thomas Bouldin <inlined@google.com>
2025-06-02 15:30:41 -0700

    Remove all memcache stuff

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const Memcached = require('memcached');
-
-if (!process.env.MEMCACHE_ADDR) {
-  console.error("MEMCACHE_ADDR must be set");
-  process.exit(1);
-
-}
-
-const cache = new Memcached(process.env.MEMCACHE_ADDR);
 
 const app = express();
-app.use(bodyParser.text());
-app.use(bodyParser.json());
 
 app.get("/cacheable", (req, res) => {
   res.set('Cache-Control', 'public, max-age=3600, s-maxage=3600');
@@ -23,27 +11,8 @@ app.get("/uncacheable", (req, res) => {
   res.json({ status: "OK" });
 });
 
-app.get("/", async (req, res) => {
-  cache.get("value", (err, data) => {
-    if (err) {
-      console.error("Failed to fetch cache value:", err);
-      res.status(500).send(err);
-      return;
-    }
-    console.log("GOT", JSON.stringify(data, null, 2));
-    res.send(data);
-  });
-});
-
-app.post("/", (req, res) => {
-  console.log("Setting value to", req.body);
-  cache.set("value", req.body, 3600, (err) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send("OK");
-    }
-  });
+app.all("/", async (req, res) => {
+  res.json({"status": "OK"});
 });
 
 const port = process.env.PORT || 8080;
